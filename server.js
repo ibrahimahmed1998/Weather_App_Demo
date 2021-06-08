c2f4c182f5d3ba48f4d36dc1119ead2a5f9942c1
ibrahim ahmed <hema.1998.man@gmail.com>
2021-06-08 05:07:22 +0200

    s

--- server.js
+++ server.js
@@ -1,68 +1,36 @@
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'); // require express javascript 
+const app = express();  
+const bodyParser = require('body-parser');  // require bodyParser from express Packages  
 /***************************/
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 /***************************/
-const cors = require('cors'); 
-app.use(cors());
+const cors = require('cors');app.use(cors());
 /***************************/
-app.use(express.static('website'));
+app.use(express.static('website'));   // require html+css+app.js files from website Directory 
 
-projectData = [];
+projectData = {};   // empty list to contain information lisk [date||temp||content"feelings"]
+const port = 8000 ;     // open port on local server : 8000 
+const server = app.listen(port,listening) ;     // listen to server on port 8000 
 
-const port = 8000 ; 
-
-const server = app.listen(port,listening) ; 
-
-function listening()
-{
-    console.log(' server runing');
-     console.log(`runing on localhost: ${port}`);
-}
+function listening(){  
+    /* if server run correctly this function return 2 log message contains : 
+    " server runing " , "runing on localhost:  8000" */ 
+    console.log(' server runing');console.log(`runing on localhost: ${port}`);}
 
 app.get('/all',sendData); // call back function : send res from server to browser 
 app.post('/addData',addData); // App send req to server  
 
 function addData(request,response)
 {
-    /*
     let data = request.body;
 
     console.log('server side data ', data)
 
-    projectData["date"] = data.date;
-    projectData["temp"] = data.temp;
-    projectData["feel"] = data.content;
-
- 
+    projectData["date"] = data.date; projectData["temp"] = data.temp; projectData["feel"] = data.content;
     response.send(projectData);
-
-    */
-
-    
-    newEntry =
-    {
-       date: request.body.date,
-       temp:request.body.temp,
-       content:request.body.content
-    }
-
-    projectData.push(newEntry);
-
-   
 }
-
-function sendData(request,response)
-{
-    console.log('data before send is : ', projectData)
-
-    response.send(projectData);
-
-    projectData = [] ;
-  
-}
-
- 
+function sendData(request,response){
+    response.send(projectData);      // send data as response from app server 
+    projectData={};}                 // make projectData Empty after send information to Display it 
--- website/app.js
+++ website/app.js
@@ -1,37 +1,41 @@
-projectData={};let days=new Date();
-let newdate=(days.getMonth()+1)+'/'+days.getDate()+'/'+days.getFullYear();
-const get="http://127.0.0.1:8000/all";
-const post="http://127.0.0.1:8000/addData";
-const URL_="http://api.openweathermap.org/data/2.5/forecast?id=";
-const KEY="&appid=1834a80ff251e3825e4b27bc56b6c633&units=metric";
+projectData={};let days=new Date(); // get Current Time from Date Function 
+let newdate=(days.getMonth()+1)+'/'+days.getDate()+'/'+days.getFullYear(); // Equality Current time in newdate container 
+const get="http://127.0.0.1:8000/all";      // this link that return response that display info 
+const post="http://127.0.0.1:8000/addData"; // this link that send request to ask about current info then return it to server   
+const URL_="http://api.openweathermap.org/data/2.5/forecast?id="; // the API of openweathermap.org site that return data
+const MyKey="&appid=1834a80ff251e3825e4b27bc56b6c633&units=metric";// my key in this site-that return temp degree in celsius
 /**********************************************************************************/
-generate=document.getElementById('generate').addEventListener("click",DO);
+generate=document.getElementById('generate').addEventListener("click",DO);//button on html page where i click on it that call DO function
 function DO(e){
-    const newZip= document.getElementById('zip').value ;                  
-    const feelings = document.getElementById('feelings').value ;          
+    const new_z_code= document.getElementById('zip').value ;   // zipcode that i want to know temprature of this area                
+    const my_feelings = document.getElementById('feelings').value ;  // random string that user enter it to say what he/she feels        
 
-    weather(URL_,newZip,KEY).then(function(data){                           
-        postData(post,{date:newdate,temp:data.list[0].main.temp,content:feelings});update();})};
-/**********************************************************************************/
-const weather = async(URL_,zip,key)=>{ // zipcode = 2643743
+    function_weather(URL_,new_z_code,MyKey).then(function(data){                           
+        postData(post,{date:newdate,temp:data.list[0].main.temp,content:my_feelings});
+        updateUI();})};
+/************************************function_weather**********************************************/
+const function_weather = async(URL_,zip,key)=>{  // example : zipcode = 2643743
     const response=await fetch(URL_+zip+key)
     try{const data=await response.json();
         return data;}catch(err){console.log("err",err);}}
-/**********************************************************************************/          
+/**********************************POST-METHOD************************************************/          
 const postData = async(url='',data={})=>{
     const response = await fetch(url,{method:'POST',credentials:'same-origin',
-    headers:{  'Content-Type':'application/json',},body:JSON.stringify(data)});
-    try{
-        const newData = await response.json();
-        console.log(newData);
+    headers:{'Content-Type':'application/json',},body:JSON.stringify(data)});
+    try{const newData = await response.json();
         return newData;}catch(err){console.log("err",err);}}
-/**********************************************************************************/
-const update = async() =>{
-    const req = await fetch(get);
-        try{ 
-        const allData = await req.json();
-        document.getElementById('content').innerHTML=`I feel:${allData[0].content}`;
-        document.getElementById('temp').innerHTML=`Temperatuer:${allData[0].temp}`;
-        document.getElementById('date').innerHTML=`Date:${allData[0].date}`;
-    }catch(err){console.log("err",err);}}
+/************************************UPDATE-UI-METHOD**********************************************/
+const updateUI = async()=>{
+    const request = await fetch(get)
+    try{
+    const allData = await request.json();
+    console.log(allData)
+    document.getElementById('date').innerHTML =` Date:${allData.date}`;
+    document.getElementById('temp').innerHTML = `Temperature:${allData.temp}`;
+    document.getElementById('content').innerHTML = `I feel:${allData.feel}`;
+    }catch(err){
+    console.log('error',err);
+    }
+    }
+            
 /**********************************************************************************/
